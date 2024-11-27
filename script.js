b22e1f607d959f6b8b716540081d22cbf32795cb
UlianaPostevka06 <valeriapalamaruk22@gmail.com>
2024-11-27 17:01:11 +0200

    Підключила до JS

---
+++ script.js
@@ -0,0 +1,15 @@
+let inst = document.querySelector('.inst')
+let tg = document.querySelector('.tg')
+let github = document.querySelector('.github')
+
+inst.addEventListener('click', function(){
+    location.assign('https://www.instagram.com/')
+})
+
+tg.addEventListener('click', function(){
+    location.assign('https://web.telegram.org/k/')
+})
+
+github.addEventListener('click', function(){
+    location.assign('https://github.com/leriksss')
+})
